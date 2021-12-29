26b33737850e7d5c51ed24acf5a4a4b2833270d6
judy-n <judynaamani@icloud.com>
2021-12-29 14:38:15 -0500

    HTTPS

--- src/index.ts
+++ src/index.ts
@@ -9,9 +9,9 @@ app.use(express.static(path.join(path.resolve(), '../static')))
 
 function requireHTTPS(req: express.Request, res: express.Response, next: express.NextFunction) {
   // The 'x-forwarded-proto' check is for Heroku
-  // if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
-  //   return res.redirect('https://' + req.get('host') + req.url);
-  // }
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
+    return res.redirect('https://' + req.get('host') + req.url);
+  }
   next();
 }
 
